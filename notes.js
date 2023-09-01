2071cdd73b7b60b5a573ea416f4351b497794cc6
mdlandsverk <140205423+mdlandsverk@users.noreply.github.com>
2023-09-01 12:21:50 +0200

    01.09.2023 12:21

--- notes.js
+++ notes.js
@@ -1,7 +1,13 @@
-const ACCESS_TOKEN = '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'; //Github API token
-const OWNER = 'DagAndreas';
-const REPO = 'UniWiki';
+// Define a constant to hold the access token for the GitHub API
+// Note: Its usually not safe to expose a access token in client-side code, however in this case I have used a github user that only have access to the specific repo and not my main user. 
+const ACCESS_TOKEN ='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';
 
+// Define constant for the owner and the repo name for the GitHub repo that we are accessing
+const OWNER = 'DagAndreas'
+const REPO = 'UniWiki'
+
+// Create an empty object to hold the sorted file contents by folder
+// Keys will be folder names and values will be arrays of file data
 let fileContentsByFolder = {};
 
 // Function to update the DOM (the webpage)
@@ -10,9 +16,12 @@ function updateDOM() {
     // Loop through each key-value pair in the fileContentsByFolder object
     for(const [folder, files] of Object.entries(fileContentsByFolder)) {
 
+        // Convert folder name to lowercase
+        const lowerCaseFolder = folder.toLowerCase();
+
         // Fetch the HTML element with an ID that matches the current folder name
-        // For example, if folder is "JavaScript", it looks for an element with the ID "JavaScript-files"
-        const folderElement = document.getElementById(`${folder}-files`);
+        // For example, if folder is "Databases", it looks for an element with the ID "databases-files"
+        const folderElement = document.getElementById(`${lowerCaseFolder}-files`);
         
         // Check if the HTML element actually exists
         if (folderElement) {
@@ -24,57 +33,53 @@ function updateDOM() {
     }
 }
 
-// Function to fetch the content of a given file and update the fileContentsByFolder object accordingly
+// Asynchronous function to fetch the content of a given .md and categorize it
 async function fetchAndUpdateFile(file) {
-    
-    // Construct the URL for fetching the file content, replacing the necessary parts and appending the access token for authorization
+
+    // Construct the URL to fetch the file content
     const fileContentUrl = file.git_url.replace('git/blobs', 'contents').concat(`?access_token=${ACCESS_TOKEN}`);
-    
-    // Fetch the content of the file from GitHub
+
+    // Fetch the file content from GitHub
     const response = await fetch(fileContentUrl);
-    
-    // Convert the fetched content into text format
-    const fileContent = await response.text();
-    
-    // Extract the folder name from the first line of the file by removing the '#' symbol
-    const folderName = fileContent.split('\n')[0].replace('#', '');
-    
-    // Check if the folder already exists in the fileContentsByFolder object
+
+    // Convert the fetched content to text
+    const fileText = await response.text();
+
+    // The first line of the .md file contains the folder name (without the "#")
+    const folderName = file.content.split('\n')[0].replace('#', '').trim();
+
+    // If the folder doesnt ecist in the object, create an empty array for it
     if (!fileContentsByFolder[folderName]) {
-        
-        // If not, initialize an empty array for that folder
         fileContentsByFolder[folderName] = [];
-        
-        // Add the file to the folder's array
-        fileContentsByFolder[folderName].push(file);
     }
+
+    // Add the file to the folder
+    fileContentsByFolder[folderName].push(file);
 }
 
-// Fetch all .md files from the GitHub repository and update the DOM accordingly
+// Asychronous function to fetch all the .md files from the GitHub repo
 async function fetchFilesFromRepo() {
 
-    // Create the URL to fetch the file tree from the GitHub repository
+    // Construct the GitHub API URL to get the tree of files
     const apiURL = `https://api.github.com/repos/${OWNER}/${REPO}/git/trees/main?recursive=1&access_token=${ACCESS_TOKEN}`;
-    
-    // Fetch the data from the GitHub API
+
+    // Fetch the JSON data from the GitHub API
     const response = await fetch(apiURL);
-    
-    // Parse the response to JSON format
+
+    // Parse the response to a JSON object
     const data = await response.json();
-    
-    // Filter out all the .md files from the file tree
+
+    // Filter to get only the .md files
     const mdFiles = data.tree.filter(file => file.path.endsWith('.md'));
-    
-    // Loop through each .md file
+
+    // Loop through each .md file and fetch its content
     for (const file of mdFiles) {
-        
-        // Fetch the file content and update the fileContentsByFolder object
         await fetchAndUpdateFile(file);
     }
 
-    // Update the DOM with the new data
+    // Update the DOM to reflect the new data
     updateDOM();
 }
 
-// Call the fetchFilesFromRepo function initially to start the process
+//Fetch files from GitHub repo when the page loads
 fetchFilesFromRepo();
